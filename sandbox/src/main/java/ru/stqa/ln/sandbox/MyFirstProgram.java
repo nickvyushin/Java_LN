984ab586bd8a8620d4b56a901a8c921d02d279fa
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-02 18:30:31 +0400

    Лекция 1.4

--- sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
@@ -4,6 +4,8 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println("Площадь квадрата со стороной " + 5 + "=" + (5 * 5));
     }
 
 }
