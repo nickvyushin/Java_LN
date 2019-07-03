46111693dad6e1dc367afdc66bc7c6995fe5db94
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-03 12:09:38 +0400

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package ru.stqa.ln.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("World");
+        hello("user");
+        hello("Nickolas");
+
+        double l = 5;
+        System.out.println("Площадь квадратат со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
     }
 
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
