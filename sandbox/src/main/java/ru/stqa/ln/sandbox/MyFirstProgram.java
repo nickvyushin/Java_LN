34fb7dbd9c6d7206b92c9dac036c411ea2ebd39c
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-03 18:10:07 +0400

    Задание №2

--- sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
@@ -12,10 +12,19 @@ public class MyFirstProgram {
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(30, 40);
+        Point p2 = new Point(10, 20);
+
+
+        System.out.println("Расстояние между точками: " + distance(p1, p2));
+        System.out.println("Расстояние между точками: " + Point.distance(p1, p2));
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
-
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt((Math.pow((p1.x - p2.x), 2)) + (Math.pow((p1.y - p2.y), 2)));
+    }
 }
