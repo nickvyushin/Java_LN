8839b34aee02b55437e9357a190cd70bbc890460
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-04 10:59:02 +0400

    Создана функция "printDistance", которая выводит сообщение орасстоянии между точками

--- sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ln/sandbox/MyFirstProgram.java
@@ -7,24 +7,32 @@ public class MyFirstProgram {
         hello("user");
         hello("Nickolas");
 
+
         Square s = new Square(5);
         System.out.println("Площадь квадратат со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+        
 
         Point p1 = new Point(30, 40);
         Point p2 = new Point(10, 20);
 
-
+        printDistance(distance(p1, p2));
         System.out.println("Расстояние между точками: " + distance(p1, p2));
         System.out.println("Расстояние между точками: " + Point.distance(p1, p2));
     }
 
+
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
+
     public static double distance(Point p1, Point p2){
         return Math.sqrt((Math.pow((p1.x - p2.x), 2)) + (Math.pow((p1.y - p2.y), 2)));
+    }
+    public static void printDistance (double distance) {
+        System.out.println("Расстояние между точками: " + distance);
+
     }
 }
