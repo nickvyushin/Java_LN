91311763e23a86e96a80ccb9bd92256c4c967e7c
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-06 10:44:53 +0400

    Исправление задание №3

--- sandbox/src/test/java/ru/stqa/ln/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/ln/sandbox/PointTests.java
@@ -3,7 +3,6 @@ package ru.stqa.ln.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static ru.stqa.ln.sandbox.MyFirstProgram.distance;
 
 public class PointTests {
 
@@ -11,6 +10,6 @@ public class PointTests {
     public void testDistance() {
         Point p1 = new Point(9, 10);
         Point p2 = new Point(4, 10);
-        Assert.assertEquals(distance(p1, p2), 5);
+        Assert.assertEquals(p1.distance(p2), 5.0);
     }
 }
