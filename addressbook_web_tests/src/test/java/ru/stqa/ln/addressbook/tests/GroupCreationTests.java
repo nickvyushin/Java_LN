d160ae9a08578331da349f1551320870ce4b58e3
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-18 15:25:56 +0400

    Добавлено лямбда-выражение для сравнения множества

--- addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.ln.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -13,9 +15,15 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("New Test", "New Test 2", "New Test 3"));
+        GroupData group = new GroupData("New Test", "New Test 2", "New Test 3");
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        //(o1, o2) -> Integer.compare(o1.getId(), o2.getId()) - эта строка заменена средой на Comparator.comparingInt(GroupData::getId)
+        group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 
