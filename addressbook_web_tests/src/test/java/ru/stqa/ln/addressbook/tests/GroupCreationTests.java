fb3b4ff4e7fac17652a03f949e27697d9060bbdc
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-29 12:53:34 +0400

    Добавлены Fluent-интерфейсы в тесты групп

--- addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupCreationTests.java
@@ -14,7 +14,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.goTo().groupPage();
         List<GroupData> before = app.group().list();
-        GroupData group = new GroupData("New Test", "New Test 2", "New Test 3");
+        GroupData group = new GroupData().withName("Test11");
         app.group().create(group);
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() + 1);
--- addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase{
     public void ensurePreconditions() {
         app.goTo().groupPage();
         if (app.group().list().size() == 0) {
-            app.group().create(new GroupData("test1", null, null));
+            app.group().create(new GroupData().withName("Test11"));
         }
     }
 
@@ -22,7 +22,8 @@ public class GroupModificationTests extends TestBase{
     public void testGroupModification() {
         List<GroupData> before = app.group().list();
         int index = before.size() - 1;
-        GroupData group = new GroupData(before.get(index).getId(), "test1", "test1", "test1" );
+        GroupData group = new GroupData()
+                .withId(before.get(index).getId()).withName("Test11").withFooter("Test22").withHeader("Test33");
         app.group().modify(index, group);
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size());
