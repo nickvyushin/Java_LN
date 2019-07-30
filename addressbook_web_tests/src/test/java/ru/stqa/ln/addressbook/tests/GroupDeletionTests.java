6ed8e055a881afeed72b01fdba9e8346a0337c8b
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-07-30 15:45:22 +0400

    Тесты модифицированы с использованием Fluent-интерфейсов

--- addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupDeletionTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupDeletionTests.java
@@ -1,11 +1,19 @@
 package ru.stqa.ln.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.ln.addressbook.model.GroupData;
+import ru.stqa.ln.addressbook.model.Groups;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase{
 
@@ -19,14 +27,13 @@ public class GroupDeletionTests extends TestBase{
 
     @Test
     public void testGroupDeletion() {
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData deletionGroup = before.iterator().next();
         app.group().delete(deletionGroup);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() - 1);
+        Groups after = app.group().all();
+        assertEquals(after.size(), before.size() - 1);
+        assertThat(before, equalTo(before.without(deletionGroup)));
 
-        before.remove(deletionGroup);
-        Assert.assertEquals(before, after);
     }
 
 }
--- addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,14 @@
 package ru.stqa.ln.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.ln.addressbook.model.GroupData;
+import ru.stqa.ln.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
+
 
 public class GroupModificationTests extends TestBase{
 
@@ -19,17 +22,14 @@ public class GroupModificationTests extends TestBase{
 
     @Test
     public void testGroupModification() {
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId()).withName("Test11").withFooter("Test22").withHeader("Test33");
         app.group().modify(group);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size());
-
-        before.remove(modifiedGroup);
-        before.add(group);
-        Assert.assertEquals(before, after);
+        Groups after = app.group().all();
+        assertEquals(after.size(), before.size());
+        assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
 
 }
