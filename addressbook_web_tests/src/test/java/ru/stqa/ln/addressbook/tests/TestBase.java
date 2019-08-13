3321be8f7f7b06d9b589e170923f9370cfcc6d97
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-08-13 15:55:20 +0400

    Вынесено описание тестового стенда в конфигурационный файл

--- addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/tests/TestBase.java
@@ -8,7 +8,7 @@ import ru.stqa.ln.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() {
