2cd77effe24e13b021fed2856f1cf2bac4179610
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-08-15 22:49:10 +0400

    Написаны тесты для извлечения информации из MySQL

--- addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/model/GroupData.java
+++ addressbook_web_tests/src/test/java/ru/stqa/ln/addressbook/model/GroupData.java
@@ -3,24 +3,36 @@ package ru.stqa.ln.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
     @XStreamOmitField
+    @Id
+    @Column(name = "group_id")
     private int id = Integer.MAX_VALUE;
+
     @Expose
+    @Column(name = "group_name")
     private String name;
+
     @Expose
+    @Column(name = "group_header")
+    @Type(type = "text")
     private String header;
+
     @Expose
+    @Column(name = "group_footer")
+    @Type(type = "text")
     private String footer;
-
-
-    public int getId() {
-        return id;
-    }
 
     public GroupData withId(int id) {
         this.id = id;
@@ -40,6 +52,10 @@ public class GroupData {
     public GroupData withFooter(String footer) {
         this.footer = footer;
         return this;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
