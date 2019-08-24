5215e4dc789e9ad045d595db735db811e9facf84
nickvyushin <52334336+nickvyushin@users.noreply.github.com>
2019-08-24 20:46:22 +0400

    Создан тест регистрации в Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/ln/mantis/appmanager/RegistrationHelper.java
@@ -0,0 +1,26 @@
+package ru.stqa.ln.mantis.appmanager;
+
+import org.openqa.selenium.By;
+
+public class RegistrationHelper extends HelperBase {
+
+    public RegistrationHelper(ApplicationManager app) {
+        super(app);
+    }
+
+    public void start(String username, String email) {
+        wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        //type(By.name());
+        click(By.cssSelector("input[value='Зарегистрироваться']"));
+    }
+
+    public void finish(String confirmationLink, String password, String name) {
+        wd.get(confirmationLink);
+        type(By.name("realname"), name);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("button[type='submit']"));
+    }
+}
